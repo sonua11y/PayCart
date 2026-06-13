abcb91da10b11e6fcab3ecb2b8421f97cf1da7dc
sonua11y <sripranathiindupalli@gmail.com>
2026-06-13 20:09:36 +0530

    Configure Render deployment

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,15 @@ app.use(express.json());
 app.use("/api/foods", foodRoutes);
 app.use("/api/payment", paymentRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(5000, () => {
-      console.log("Server Running");
+
+    app.listen(PORT, () => {
+      console.log(`Server Running on Port ${PORT}`);
     });
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
